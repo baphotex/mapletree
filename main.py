6d619486333ace0d195b1d5cf0673342c6b0661d
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-11 04:13:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,38 @@ import os
 
 app = Flask(__name__,  static_folder='static')
 
-@app.route('/')
+def ep(start, end, ep_display = False, low = 12, high = 25):
+    y = list(range(start,end+1))
+    z = len(y)
+    string = f'''
+    There are {z} episodes left.
+    It will take {z*24/60} to {z*30/60} hours to finish watching.
+    It will take {round(z*24/60/24,2)} to {round(z*30/60/24,2)} days to finish watching
+    Based on current habits, it will take {round(z/high,2)} to {round(z/low,2)} days to finish.''' 
+    return string 
+
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name + last_name
+    return render_template("form.html")
+@app.route('/', methods =["GET", "POST"])
 def index():
-    return render_template("maple_home.html")
+    if request.method == "POST":
+        start = request.form.get("startep")
+        end = request.form.get("endep") 
+        try: 
+            start = int(start)
+            end = int(end)
+            string = ep(start,end) 
+        except TypeError as e: 
+            string = "Uh oh! Those weren't the right kind of numbers :(" 
+        return render_template("maple_home.html", response = string) 
+    return render_template("maple_home.html", response = None)
 
 @app.route('/favicon.ico')
 def favicon():
