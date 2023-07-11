9083b8a054d7cbcde0574114ad213d5453c5e344
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-10 20:49:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return render_template("home.html")
+        return render_template("/main.html")
     except Exception as e:
         return str(e)
     #return "Sweet" 
     #return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-
+@app.route('/case')
+def case():
+    return render_template("/temp.html")
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
