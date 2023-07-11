eda0630efd2f2b2ccdb40a33f75a4cbb10160d0b
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-11 04:16:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,6 @@ def ep(start, end, ep_display = False, low = 12, high = 25):
     return string 
 
 @app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
-    return render_template("form.html")
-@app.route('/', methods =["GET", "POST"])
 def index():
     if request.method == "POST":
         start = request.form.get("startep")
