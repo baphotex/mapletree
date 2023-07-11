e282ee569afb8f678f419317d89e7ab654d9d667
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-10 21:37:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['FAVICON'] = '/maple.ico'
 
 @app.route('/')
 def index():
-    return render_template("angel.html")
+    return render_template("rain.html")
     #try:
     #    with open("home.html") as f:
     #        html = f.read()
