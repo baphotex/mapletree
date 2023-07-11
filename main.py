4a09bae94a6bc99ad64c7269945b09fd182122df
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-11 00:24:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,6 @@ def bio():
 def blog():
     return render_template("blog_cute.html") 
  
-@app.route('/test4') 
-def test4():
-    return render_template("adorable_four.html")
-
-@app.route('/test5') 
-def test5():
-    return render_template("adorable_five.html")
-
 @app.route('/test6') 
 def test6():
     return render_template("adorable_six.html")
@@ -32,12 +24,6 @@ def test6():
 @app.route('/test7') 
 def test7():
     return render_template("adorable_seven.html")
-    
-@app.route('/case')
-def case():
-    with open("temp.html") as f:
-        html = f.read()
-    return html
-    #return render_template("temp.html")
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
